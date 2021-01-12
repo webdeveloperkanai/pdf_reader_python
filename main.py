a753b1418ee74ab5cff9575e0ad26f4c18c6f374
webdevkanai <kanaishil501@gmail.com>
2021-01-12 15:56:39 +0530

    Python Pdf Reader

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import PyPDF2
+name = input("Enter pdf name and it should be in same dir : ")
+no = int(input("Enter starting page : "))
+book = open(name,'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+sp = pyttsx3.init()
+for num in range (no,pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    sp.say(text)
+    sp.runAndWait()
